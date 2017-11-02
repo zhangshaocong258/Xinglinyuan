5b1a5e71f5e03ef736ce583410f7123ae3bdb324
zhangshaocong258 <597001494@qq.com>
2017-11-02 10:34:25 +0800

    tree分词

---
+++ src/main/java/tree/util/ObjectBean.java
@@ -0,0 +1,13 @@
+package tree.util;
+
+public class ObjectBean {
+	public static int getInt(String str , int def){
+		try {
+			return Integer.parseInt(str) ;
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return def ;
+		}
+	}
+}
