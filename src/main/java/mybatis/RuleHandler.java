06246abdff47ddcbe7b72eef6cc57c509b0d63e5
zhangshaocong258 <597001494@qq.com>
2017-11-18 00:25:56 +0800

    数据库

--- src/main/java/mybatis/RuleHandler.java
+++ src/main/java/mybatis/RuleHandler.java
@@ -14,78 +14,113 @@ import java.util.List;
  */
 public class RuleHandler {
     public static void main(String args[]) {
-//        select();
+//        selectById();
+//        selectByName("x");
 //        selectAll();
 //        insert();
-        insertAll();
+//        insertAll();
 //        update();
 //        remove();
 
     }
-    public static void select() {
+    public static void selectById(int id) {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao ruleDaoMapper = sqlSession.getMapper(RuleDao.class);
-        Rule rule = ruleDaoMapper.selectRuleById(7);
-        System.out.println(rule.getZhumai());
+        try {
+            RuleDao ruleDaoMapper = sqlSession.getMapper(RuleDao.class);
+            Rule rule = ruleDaoMapper.selectRuleById(id);
+            System.out.println(rule.getZhumai());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    public static Rule selectByName(String name) {
+        SqlSession sqlSession = getSessionFactory().openSession();
+        Rule rule;
+        try {
+            RuleDao ruleDaoMapper = sqlSession.getMapper(RuleDao.class);
+            rule = ruleDaoMapper.selectRuleByName(name);
+        } finally {
+            sqlSession.close();
+        }
+        return rule;
     }
 
     public static void selectAll() {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
-        List<Rule> rule = ruleMapper.selectAllRules();
-        System.out.println(rule.get(0).getId());
+        try {
+            RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
+            List<Rule> rule = ruleMapper.selectAllRules();
+            System.out.println(rule.get(0).getId());
+        } finally {
+            sqlSession.close();
+        }
     }
 
     public static void insert() {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
-        Rule rule = new Rule(1, "aa","zsc", "11", "faff", "11", "faff", "aa", "aa","aa");//xml中设置是否自动编号
-        ruleMapper.insertRule(rule);
-        sqlSession.commit();
+        try {
+            RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
+            Rule rule = new Rule(1, "aa","zsc", "11", "faff", "11", "faff", "aa", "aa","aa");//xml中设置是否自动编号
+            ruleMapper.insertRule(rule);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
     }
 
     public static void insertAll() {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao RuleMapper = sqlSession.getMapper(RuleDao.class);
-        //                        心气虚,     主症,   次证,    主舌质,    次舌质,    主舌苔,    次舌苔,    主脉,   次脉
-        Rule rule1 = new Rule(1, "x","!aaw,aba,adr,acv,aar","aau,adq,add,adl,ado,ads,adp,aay,aaz",
-                "淡白", "淡红,胖,嫩,淡白",
-                "白,润", "薄,白,腻",
-                "虚","细,弱,涩,沉,缓,结,代");//心气虚
-        Rule rule2 = new Rule(2, "g","!aaz,!aau,!aax,@abn,@acj,@abm,@ack,@acl,@acd,@aci,#adj,#aao,#aas,#ade,#ads",
-                "adc,aar,aas,aak,aal,aam,adg,adf,adk,aau,aav,aaf,aab,aad,aae,aax,adp,aao,aan,aaz,abm,adr,abp,act,acs,acw,acx,acy,acl,aco,acp,aau,add,acg,ach,abq,abz,acb,aby,aca,abw",
-                "红", "红,淡红,裂,绛",
-                "少", "少津,少,薄,白,黄,无",
-                "弦,细,数","数,沉,细,弦");//肝肾
-        Rule rule3 = new Rule(3, "p","aap,abl,abp,abx,acv",
-                "aar,aat,adn,adm,adq,add,abp,abd,abq,abx,adb,aag,abx,aaj,abc,abv,adc,acv,acz,aaq,abz,aby,aca,adk,adf,ace,aci,acg,aci",
-                "淡白,胖,嫩,齿", "紫,胖,嫩",
-                "白", "白,腻,润",
-                "缓,弱","虚,沉,细,弱,数");//脾胃
-        List<Rule> list = new ArrayList<Rule>();
-        list.add(rule1);
-        list.add(rule2);
-        list.add(rule3);
-        RuleMapper.insertRule(rule1);
-        RuleMapper.insertRule(rule2);
-        RuleMapper.insertRule(rule3);
-        sqlSession.commit();
+        try {
+            RuleDao RuleMapper = sqlSession.getMapper(RuleDao.class);
+            //                        心气虚,     主症,   次证,    主舌质,    次舌质,    主舌苔,    次舌苔,    主脉,   次脉
+            Rule rule1 = new Rule(1, "x","!aaw,aba,adr,acv,aar","aau,adq,add,adl,ado,ads,adp,aay,aaz",
+                    "淡白", "淡红,胖,嫩,淡白",
+                    "白,润", "薄,白,腻",
+                    "虚","细,弱,涩,沉,缓,结,代");//心气虚
+            Rule rule2 = new Rule(2, "g","!aaz,!aau,!aax,@abn,@acj,@abm,@ack,@acl,@acd,@aci,#adj,#aao,#aas,#ade,#ads",
+                    "adc,aar,aas,aak,aal,aam,adg,adf,adk,aau,aav,aaf,aab,aad,aae,aax,adp,aao,aan,aaz,abm,adr,abp,act,acs,acw,acx,acy,acl,aco,acp,aau,add,acg,ach,abq,abz,acb,aby,aca,abw",
+                    "红", "红,淡红,裂,绛",
+                    "少", "少津,少,薄,白,黄,无",
+                    "弦,细,数","数,沉,细,弦");//肝肾
+            Rule rule3 = new Rule(3, "p","!aap,!abl,abp,abx,acv",
+                    "aar,aat,adn,adm,adq,add,abp,abd,abq,abx,adb,aag,abx,aaj,abc,abv,adc,acv,acz,aaq,abz,aby,aca,adk,adf,ace,aci,acg,aci",
+                    "淡白,胖,嫩,齿", "紫,胖,嫩",
+                    "白", "白,腻,润",
+                    "缓,弱","虚,沉,细,弱,数");//脾胃
+            List<Rule> list = new ArrayList<Rule>();
+            list.add(rule1);
+            list.add(rule2);
+            list.add(rule3);
+            RuleMapper.insertAllRule(list);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
     }
 
 
     public static void update() {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
-        Rule rule = ruleMapper.selectRuleById(7);
-        ruleMapper.updateRule(rule);
-        sqlSession.commit();
+        try {
+            RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
+            Rule rule = ruleMapper.selectRuleById(7);
+            ruleMapper.updateRule(rule);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
     }
 
     public static void remove() {
         SqlSession sqlSession = getSessionFactory().openSession();
-        RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
-        ruleMapper.deleteRule(1);
-        sqlSession.commit();
+        try {
+            RuleDao ruleMapper = sqlSession.getMapper(RuleDao.class);
+            ruleMapper.deleteRule(1);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
     }
 
     //Mybatis 通过SqlSessionFactory获取SqlSession, 然后才能通过SqlSession与数据库进行交互
